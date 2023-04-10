ef5975651a38ffcfdfa9f9bfcf51093ec1f9e3b2
Elwoodbond <elwood.bond@gmail.com>
2023-04-10 19:17:08 -0400

    installed next js

---
+++ src/app/head.jsx
@@ -0,0 +1,12 @@
+export default function Head() {
+    return (
+        <Head>
+        <meta charSet="utf-8" />
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="This is an IMDB clone website" />
+        <link rel="icon" href="/favicon.ico" />
+        <title>IMDB Clone</title>
+        </Head>
+    )
+    }
