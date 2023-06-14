18c626751e26d19b9f8cfc7f9242c432a12280b6
Elwood Bond <elwood.bond@gmail.com>
2023-06-14 16:13:08 -0400

    created Navbar component and styled

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,5 +1,5 @@
 export default function About() {
-    return <div className="max-w-6xl mx-auto space-y-4">
+    return <div className="max-w-6xl mx-auto space-y-4 p-4">
         <h1 className="text-2xl font-medium text-amber-600">About</h1>
         <p className="text-gray-500">IMDb is an online database of information related to films, television series, podcasts, home videos, video games, and streaming content online – including cast, production crew and personal biographies, plot summaries, trivia, ratings, and fan and critical reviews.</p>
     </div>
