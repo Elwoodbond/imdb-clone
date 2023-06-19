0e91df74a800e421397685d8c3439811a0ff2e7a
Elwood Bond <elwood.bond@gmail.com>
2023-06-19 19:19:56 -0400

    add loading effect

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,6 +1,12 @@
 export default function About() {
     return <div className="max-w-6xl mx-auto space-y-4 p-4">
         <h1 className="text-2xl font-medium text-amber-600">About</h1>
-        <p className="text-gray-500">IMDb is an online database of information related to films, television series, podcasts, home videos, video games, and streaming content online – including cast, production crew and personal biographies, plot summaries, trivia, ratings, and fan and critical reviews.</p>
+        <p className="text-gray-500">IMDb (Internet Movie Database) is an online database that serves as a comprehensive source of information about films, television shows, actors, directors, and other industry professionals. It provides a platform for users to explore a vast collection of movie-related data, including cast and crew details, plot summaries, trivia, reviews, ratings, trailers, and more.</p>
+
+        <p className="text-gray-500">With millions of entries spanning various genres and languages, IMDb is a go-to resource for movie enthusiasts, industry insiders, and casual viewers alike. It offers a user-friendly interface that allows visitors to search for specific titles, browse through popular films, and discover new releases. Each film or TV show page typically includes essential details such as the release date, runtime, genre, and a brief synopsis, providing users with a quick overview.</p>
+
+        <p className="text-gray-500">In addition to providing factual information, IMDb also features user-generated content. Registered users can contribute by submitting reviews, ratings, and trivia, creating personalized watchlists, and participating in discussions on message boards. This interactive aspect allows individuals to share their opinions, recommendations, and insights, fostering a vibrant community of film enthusiasts.</p>
+
+        <p className="text-gray-500">IMDb's extensive database is continuously updated to include new releases, upcoming projects, and comprehensive information on both mainstream and independent productions. It also features supplementary content like celebrity biographies, filmographies, and awards information, making it a valuable resource for researching the careers and accomplishments of industry professionals.</p>
     </div>
 }
